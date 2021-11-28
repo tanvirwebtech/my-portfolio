53bcc66d49746c5e35aee65f8683ba4c8cb1abc9
tanvirwebtech <tanvirahmed9593@gmail.com>
2021-11-29 00:05:14 +0600

    project start

---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import { Button, Box } from "@mui/material";
+
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <Box>
+                <Button variant="contained">Click</Button>
+            </Box>
+            <div>Home</div>
+        </>
+    );
+};
+
+export default Home;
+
+/* 
+
+ <a
+                href="./tanvir-ahmed-full-stack-developer.pdf"
+                download="tanvir-resume"
+            >
+                <button>Download</button>
+            </a>
+*/
